e18eeb9a76806ec7ee7ff39c0bba3f31ddf496c5
hanzhihua <hk90917@163.com>
2021-06-21 14:56:14 +0800

    second commit

--- ems_vue/src/test/java/com/test.java
+++ ems_vue/src/test/java/com/test.java
@@ -2,6 +2,6 @@ package com;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello,word");
+        System.out.println("hello,word，版本2.0");
     }
 }
